Backtesting: Fix bugs in Omega_(t+1) calculation and add named arguments in db

backtesting/garch_utilites.py
@@ -4,42 +4,43 @@ from numpy.linalg import multi_dot as mdot
 from numpy import dot
 
 
-def main_loop(out_of_sample, sigmas, epsilons, Qbar, Q_t, **kw):
-    initial_value_check(out_of_sample, sigmas, epsilons, Qbar, Q_t, **kw)
+def main_loop(out_of_sample_returns, sigmas, epsilons, Qbar, Q_t, **kw):
+    initial_value_check(out_of_sample_returns, sigmas, epsilons, Qbar, Q_t, **kw)
     if kw['kappa'] is not None:  # Model is gjrGARCH11
         print('gjrGARCH11 detected')
     Omega_ts = []
     p = np.size(sigmas, 1)
-    for t, r_t in enumerate(out_of_sample.values):
+
+    # For t = T to M, where T is first in-sample period
+    # t period values are always known
+    # t+1 period values are always forecasted
+
+    for t, r_t in enumerate(out_of_sample_returns.values):
         r_t = np.reshape(r_t, (p, 1))
-        # 0. Get current sigma^2
+
+        # Get variables from current period for all N assets
         s_t_Sq = np.reshape(sigmas[-1], (p, 1))
-
-        # 1. Calculate current period epsilon
         e_t, e_t_Sq, epsilons = calc_epsilon(epsilons, r_t, kw['mu'])
+        s_t_Sq_plus_1 = calc_sigma(e_t_Sq, s_t_Sq, e=e_t, **kw)
 
-        # 2. Calculate for all assets sigma_^2
-        s_t_Sq_plus_1 = calc_sigma(e_t_Sq, s_t_Sq, e_t_1=e_t, **kw)
-
-        # 3. Calculate Var_t, Var_t_inv
         Var_t, Var_t_inv = calc_Var_t(s_t_Sq)
-        Var_t_plus_1, Var_t_plus_1_inv = calc_Var_t(s_t_Sq_plus_1)
-
-        # 4. Calculate eta_t
         eta_t = dot(Var_t_inv, e_t)
 
+        # Construct Omega_(t+1) from F_t measurable variables
+        Var_t_plus_1, Var_t_plus_1_inv = calc_Var_t(s_t_Sq_plus_1)
+
         # 6. Calculate Q_(t+1)
-        Q_t_plus_1, Q_t_plus_1_s_inv = calc_Q_t_plus_1(Qbar, Q_t, eta_t, **kw)
+        Q_t_plus_1, Q_t_plus_1_s_inv = calc_Q_t_plus_1(Qbar=Qbar, Q_t=Q_t, eta_t=eta_t, **kw)
 
         # 7. Calulate Gamma_t
-        Gamma_t_plus_1 = calc_Gamma_t_plus_1(Q_t_plus_1_s_inv, Q_t_plus_1)
+        Gamma_t_plus_1 = calc_Gamma_t_plus_1(Q_t_plus_1_s_inv=Q_t_plus_1_s_inv, Q_t_plus_1=Q_t_plus_1)
 
         # 8. Calculate Omega_(t+1)
-        Omega_t_plus_1 = calc_Omega_t_plus_1(Var_t_plus_1, Gamma_t_plus_1)
+        Omega_t_plus_1 = calc_Omega_t_plus_1(Var_t_plus_1=Var_t_plus_1, Gamma_t_plus_1=Gamma_t_plus_1)
 
-        # Storing Omega_t
+        # Storing Omega_t and sigmas
         Omega_ts.append(Omega_t_plus_1)
-        _sigmas = np.append(sigmas, np.reshape(s_t_Sq, (1, p)), axis=0)
+        sigmas = np.append(sigmas, np.reshape(s_t_Sq_plus_1, (1, p)), axis=0)
 
         # Iterate one period
         Q_t = Q_t_plus_1
@@ -66,26 +67,26 @@ def values_from_last_period(epsilons, sigmas):
 
 
 def calc_Qbar(epsilons, sigmas):
-    eta = []
+    eta = np.empty(epsilons.shape)
     p = np.size(epsilons, 1)
-    for epsilon_t, sigma_t in zip(epsilons, sigmas):
+    for i, (epsilon_t, sigma_t) in enumerate(zip(epsilons, sigmas)):
         epsilon_t = np.reshape(epsilon_t, (p, 1))
         Var_t, Var_t_inv = inv(calc_Var_t(sigma_t))
-        eta.append(dot(Var_t_inv, epsilon_t))
+        eta_dot = dot(Var_t_inv, epsilon_t).T
+        eta[i] = eta_dot
 
-    eta = np.array(eta)
-    Qbar = 1 / len(epsilons) * sum([dot(eta, eta.T) for eta in eta])
+    Qbar = 1 / len(epsilons) * sum([dot(np.reshape(eta, (p, 1)), np.reshape(eta, (1, p))) for eta in eta])
     assert np.size(Qbar, 1) == np.size(epsilons, 1)
     return Qbar
 
 
-def calc_epsilon(epsilons, r_t, mu):
+def calc_epsilon(epsilons, returns, mu):
     p = np.size(epsilons, 1)
     # Ensure e_t is px1
-    e_t = np.reshape(np.array([r_t - mu]).T, (p, 1))
-    e_t_Sq = np.square(e_t)
-    epsilons = np.append(epsilons, e_t.T, axis=0)
-    return e_t, e_t_Sq, epsilons
+    e = np.reshape(np.array([returns - mu]).T, (p, 1))
+    e_Sq = np.square(e)
+    epsilons = np.append(epsilons, e.T, axis=0)
+    return e, e_Sq, epsilons
 
 
 def indicator_func(x):
@@ -97,14 +98,14 @@ def arr_indicator_func(value_arr, indicator_arr):
     return res
 
 
-def calc_sigma(e_t_1_Sq, s_t_1_Sq, e_t_1=None, **kw):
+def calc_sigma(e_Sq, s_Sq, e=None, **kw):
     if kw['kappa'] is not None:  # in case of a gjrGARCH(1, 1) model
-        assert (e_t_1 is not None)
+        assert (e is not None)
         next_sigma = np.array(
-            [kw['omega'] + kw['alpha'] * e_t_1_Sq +
-             kw['beta'] * s_t_1_Sq + kw['kappa'] * arr_indicator_func(e_t_1_Sq, e_t_1)])
+            [kw['omega'] + kw['alpha'] * e_Sq +
+             kw['beta'] * s_Sq + kw['kappa'] * arr_indicator_func(e_Sq, e)])
     else:
-        next_sigma = np.array([kw['omega'] + kw['alpha'] * e_t_1_Sq + kw['beta'] * s_t_1_Sq])
+        next_sigma = np.array([kw['omega'] + kw['alpha'] * e_Sq + kw['beta'] * s_Sq])
     return next_sigma
 
 