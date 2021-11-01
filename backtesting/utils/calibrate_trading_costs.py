1a1a2ce6039dfb474e94a9ddb0b9ec199c858d81
neriksen <nielseriksen@me.com>
2021-11-01 11:37:48 +0100

    Backtesting: Finalize calibrate trading costs

--- backtesting/utils/calibrate_trading_costs.py
+++ backtesting/utils/calibrate_trading_costs.py
@@ -1,8 +1,10 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
 
 
 def calc_gammaD():
+    portfolio_value = 1e9
     assets = {"EEM": "Emerging Markets",
               "IVV": "S&P 500",
               "IEV": "MSCI Europe",
@@ -19,14 +21,27 @@ def calc_gammaD():
     data = yf.download(tickers, start="2008-01-01", end="2017-10-11")
     avgvol_std = pd.concat([data['Volume'].mean(),
                             data['Adj Close'].mean(),
-                            data['Adj Close'].pct_change().std()*data['Adj Close'].mean()], axis=1)
-    avgvol_std.columns = ["Avg volume", "Avg price", "Price change vol"]
+                            data['Adj Close'].pct_change().std(),
+                            data['Adj Close'].pct_change().std()*data['Adj Close'].mean(),
+                            data['Volume'].mean()*data['Adj Close'].mean()],
+                           axis=1)
+    avgvol_std.columns = ["Avg volume", "Avg price", "Avg vol", "Price change vol", "Avg dollar volume"]
 
     # See Pedersen 2013 page 2328
-    avgvol_std['gammaD'] = (1/(0.0159*avgvol_std['Avg volume']*(avgvol_std['Price change vol']**2)))*2*(0.001*avgvol_std['Avg price'])
-
+    avgvol_std['gammaD'] = (avgvol_std['Avg vol']**2)*((avgvol_std['Avg volume']*0.0159*avgvol_std['Avg price'])**(-1))*0.001*portfolio_value
+    print(avgvol_std['gammaD'].mean())
+    print(avgvol_std['gammaD'].median())
     avgvol_std.to_csv('avg_volume.csv')
 
 
+def asset_lookup(asset_names: list, col_lookup):
+    """
+    Possible col_lookup names: Avg volume, Avg price, Price change vol, gammaD
+    """
+    df = pd.read_csv('backtesting/utils/avg_volume.csv', index_col=0)
+    return df[col_lookup].loc[[*asset_names]]
+
+
 if __name__ == '__main__':
+    #print(avg_vol_lookup(['EEM', 'EXI', 'TLT']))
     calc_gammaD()
