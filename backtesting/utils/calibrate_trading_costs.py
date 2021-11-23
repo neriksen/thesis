960a4040eeba572aed2ad3be76306e58e5131003
neriksen <nielseriksen@me.com>
2021-11-23 13:45:57 +0100

    Calibration: Switch completely to dollar terms

--- backtesting/utils/calibrate_trading_costs.py
+++ backtesting/utils/calibrate_trading_costs.py
@@ -4,7 +4,6 @@ import os
 
 
 def calc_gammaD():
-    portfolio_value = 1e9
     assets = {"EEM": "Emerging Markets",
               "IVV": "S&P 500",
               "IEV": "MSCI Europe",
@@ -19,16 +18,18 @@ def calc_gammaD():
     tickers = list(assets.keys())
 
     data = yf.download(tickers, start="2008-01-01", end="2017-10-11")
+    data.loc[:, ('Volume', ['BZ=F', 'GC=F'])] *= 100     # To account for contract size of 100 on futures
     avgvol_std = pd.concat([data['Volume'].mean(),
                             data['Adj Close'].mean(),
-                            data['Adj Close'].pct_change().std(),
-                            data['Adj Close'].pct_change().std()*data['Adj Close'].mean(),
+                            data['Adj Close'].pct_change().var(),
+                            (data['Adj Close'].pct_change().std()*data['Adj Close'].mean())**2,
                             data['Volume'].mean()*data['Adj Close'].mean()],
                            axis=1)
-    avgvol_std.columns = ["Avg volume", "Avg price", "Avg vol", "Price change vol", "Avg dollar volume"]
+    avgvol_std.columns = ["Avg volume", "Avg price", "Avg var", "Price change var", "Avg dollar volume"]
 
     # See Pedersen 2013 page 2328
-    avgvol_std['gammaD'] = (avgvol_std['Avg vol']**2)*((avgvol_std['Avg volume']*0.0159*avgvol_std['Avg price'])**(-1))*0.001*portfolio_value
+    #  Dollar gamma_D
+    avgvol_std['gammaD'] = ((avgvol_std['Avg volume']*(0.0159*avgvol_std['Price change var']))**(-1))*0.001*avgvol_std['Avg price']
     print(avgvol_std['gammaD'].mean())
     print(avgvol_std['gammaD'].median())
     avgvol_std.to_csv(os.path.join(os.path.dirname(__file__), 'avg_volume.csv'))
@@ -42,13 +43,11 @@ def asset_lookup(asset_names: list, col_lookup):
     return df[col_lookup].loc[[*asset_names]]
 
 
-def get_gamma_D(portfolio_value):
-    median_gamma_D_dict = {1e9: 0.0008471730344743024,
-                    1e8: 8.471737930382345e-05}
-    mean_gamma_D_dict = {1e9: 0.005520315335166648,
-                    1e8: 0.0005520316111414786}
-    assert (portfolio_value in median_gamma_D_dict.keys())
-    return median_gamma_D_dict[portfolio_value]
+def get_gamma_D(mean_or_median):
+    gamma_D_dict = {"mean": 1.526988705495546e-06,
+                    "median": 8.471737930382345e-05}
+    assert (mean_or_median in gamma_D_dict.keys())
+    return gamma_D_dict[mean_or_median]
 
 
 if __name__ == '__main__':
