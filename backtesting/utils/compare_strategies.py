540ca0a95a8953c8e804e55a1213d8826199fbda
neriksen <nielseriksen@me.com>
2021-12-09 20:49:34 +0100

    Backtesting: Revert calculation of Omega_T

--- backtesting/utils/compare_strategies.py
+++ backtesting/utils/compare_strategies.py
@@ -37,7 +37,7 @@ def calc_transaction_costs(weights: pd.DataFrame, returns, Omega_ts, portfolio_v
     for t, (v_t, Omega_t, r_t, r_t_1) in enumerate(zip(weights.iterrows(), Omega_ts, returns.shift(1, fill_value=0).iterrows(),
                                                        returns.shift(2, fill_value=0).iterrows())):
 
-        Omega_value = Omega_t[1]/10000
+        Omega_value = Omega_t[1]
 
         v_t_value = np.reshape(v_t[1].values, (p, 1))
         r_t_value = np.reshape(r_t[1].values, (p, 1))
